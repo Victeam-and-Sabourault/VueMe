f3d03adb0e729ee8fffe39613bab70794b8acc27
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2017-05-13 19:30:53 +0200

    ADD: common header and voice recognizer

--- src/commun/VoiceRecognizer.vue
+++ src/commun/VoiceRecognizer.vue
@@ -1,12 +1,163 @@
 <template>
-  <div>
-  </div>
+<div class="voice-recognizer-container">
+    <div v-if="isSupported">
+        <button class="ic-voice-container" @click="listen">
+            <img class="ic-voice" src="../assets/icons/ic_voice.svg">
+        </button>
+        <p v-if="transcription" class="transcription">{{ transcription }}</p>
+    </div>
+    <div v-else>
+        Your browser doesn't support speech recognition 😢
+    </div>
+</div>
 </template>
 
 <script>
+import { ApiAiClient } from 'api-ai-javascript/ApiAiClient'
+
+const client = new ApiAiClient({accessToken: '53002c34bea24a65afd849611e96531e'});
+
 export default {
-}
+        name: 'VueJS',
+        data () {
+            return {
+                isSupported: true,
+                isListening: false,
+                recognizer: null,
+                transcription: '',
+            }
+        },
+        created () {
+            // Test browser 
+            window.SpeechRecognition = window.SpeechRecognition ||
+                window.webkitSpeechRecognition  ||
+                null;
+            if (window.SpeechRecognition === null) {
+                this.isSupported = false;
+            } else {
+                this.recognizer = new window.SpeechRecognition();
+                this.recognizer.lang = 'fr-FR';
+                // Recogniser doesn't stop listening even if the user pauses
+                this.recognizer.continuous = true;
+                // interim results
+                this.recognizer.interimResults = false;
+                // Start recognising
+                this.recognizer.onresult = (event) => {
+                    this.transcription = '';
+                    for (let result of event.results) {
+                      if(result.isFinal) {
+                        this.transcription = result[0].transcript;
+                        this.sendAction(result[0].transcript);
+                      } else {
+                        this.transcription += result[0].transcript;
+                      }
+                    }
+                };
+            }
+        },
+        methods: {
+            listen () {
+                if (this.isListening) {
+                    this.stop();
+                } else {
+                    this.play();
+                }
+                this.isListening = !this.isListening;
+            },
+            play () {
+                try{
+                  this.recognizer.start();
+                  console.log('Recognition started');
+               } catch(e) {
+                  console.log(e.message);
+               }
+            },
+            stop () {
+                this.recognizer.stop();
+                console.log('Recognition stopped');
+            },
+            sendAction () {
+              console.log(this.transcription);
+              if (this.transcription.length < 1) {
+                this.transcription = 'Ok cool'
+              }
+              client.textRequest(this.transcription)
+                .then(resp => this.handleResponse(resp))
+                .then(() => { setTimeout(() => this.transcription = '', 2000) })
+                .catch(err => handleError(err))
+
+            },
+            handleResponse(res) {
+              console.log(res)
+              if (res.result.action === 'turnOn') {
+                // send action to turn on light
+              }
+              var msg = new SpeechSynthesisUtterance(res.result.fulfillment.speech);
+              msg.lang = 'fr-FR';
+              window.speechSynthesis.speak(msg);
+            },
+            handleError (err) {
+              this.transcription = '';
+              console.log(err)
+            } 
+        }
+
+    }
 </script>
 
-<style>
+<style scoped>
+.flex {
+    display: flex;
+}
+
+.voice-recognizer-container {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.ic-voice-container {
+    padding: 0;
+    border-radius: 50%;
+    background: #465565;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.78);
+    margin: 16px;
+    height: 56px;
+    width: 56px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    box-sizing: border-box;
+}
+
+.ic-voice-container.active {
+    background-color: #6FB485;
+}
+
+.ic-voice-container:focus {
+    outline: 0;
+}
+
+.ic-voice-container:active {
+    background-color: #29333D;
+}
+
+.ic-voice {
+    height: 56px;
+    width: 56px;
+}
+
+.transcription {
+    text-align: center;
+    font-family: Arial;
+    padding: 5px 10px;
+    background: rgba(0,0,0,0.80);
+    color: white;
+    border-radius: 20px;
+}
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,110 +1,13 @@
 <template>
 <div>
-    <div v-if="isSupported">
-        <button @click="listen">LISTEN</button>
-        <p>{{ transcription }}</p>
-    </div>
-    <div v-else>
-        Your browser doesn't support speech recognition 😢
-    </div>
     <router-link to="/eclairage">Go to Eclairage</router-link>
     <router-link to="/robotique">Go to Robotique</router-link>
 </div>
 </template>
 
 <script>
-import { ApiAiClient } from 'api-ai-javascript/ApiAiClient'
 
-const client = new ApiAiClient({accessToken: '53002c34bea24a65afd849611e96531e'});
-
-export default {
-        name: 'VueJS',
-        data () {
-            return {
-                isSupported: true,
-                isListening: false,
-                recognizer: null,
-                transcription: '',
-            }
-        },
-        created () {
-            // Test browser 
-            window.SpeechRecognition = window.SpeechRecognition ||
-                window.webkitSpeechRecognition  ||
-                null;
-            if (window.SpeechRecognition === null) {
-                this.isSupported = false;
-            } else {
-                this.recognizer = new window.SpeechRecognition();
-                this.recognizer.lang = 'fr-FR';
-                // Recogniser doesn't stop listening even if the user pauses
-                this.recognizer.continuous = true;
-                // interim results
-                this.recognizer.interimResults = false;
-                // Start recognising
-                this.recognizer.onresult = (event) => {
-                    this.transcription = '';
-                    for (let result of event.results) {
-                      if(result.isFinal) {
-                        this.transcription = result[0].transcript;
-                        this.sendAction(result[0].transcript);
-                      } else {
-                        this.transcription += result[0].transcript;
-                      }
-                    }
-                };
-            }
-        },
-        methods: {
-            listen () {
-                if (this.isListening) {
-                    this.stop();
-                } else {
-                    this.play();
-                }
-                this.isListening = !this.isListening;
-            },
-            play () {
-                try{
-                  this.recognizer.start();
-                  console.log('Recognition started');
-               } catch(e) {
-                  console.log(e.message);
-               }
-            },
-            stop () {
-                this.recognizer.stop();
-                console.log('Recognition stopped');
-            },
-            sendAction () {
-              console.log(this.transcription);
-              if (this.transcription.length < 1) {
-                this.transcription = 'Ok cool'
-              }
-              client.textRequest(this.transcription)
-                .then(resp => this.handleResponse(resp))
-                .then(() => { setTimeout(() => this.transcription = '', 2000) })
-                .catch(err => handleError(err))
-
-            },
-            handleResponse(res) {
-              console.log(res)
-              if (res.result.action === 'turnOn') {
-                // send action to turn on light
-              }
-              var msg = new SpeechSynthesisUtterance(res.result.fulfillment.speech);
-              msg.lang = 'fr-FR';
-              window.speechSynthesis.speak(msg);
-            },
-            handleError (err) {
-              this.transcription = '';
-              console.log(err)
-            } 
-        }
-
-    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
