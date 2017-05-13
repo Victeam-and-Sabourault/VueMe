d0f7b8bcb472cff95e7b1d1c7d47083abf60df55
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2017-05-13 18:16:48 +0200

    DEL: style on body

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  background-color: #F5F5F5;
 }
 
-body {
+html, body {
   min-height: 100vh;
   margin: 0;
+  background-color: #F5F5F5;
 }
 </style>
