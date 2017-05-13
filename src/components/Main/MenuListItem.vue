142e40cf0a7c7a890ad4f2722e26bfeb2a36f164
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2017-05-13 22:55:48 +0200

    ADD: menu list items

--- src/components/Main/MenuListItem.vue
+++ src/components/Main/MenuListItem.vue
@@ -1,23 +1,107 @@
 <template>
-<div>
-    OK COOL
-</div>
+<router-link :to="route" tag="li" class="menu-list-item">
+    <div class="menu-item-icon-section">
+        <img :src="src"/>
+    </div>
+    <div class="menu-item-content">
+        <h3>{{title}}</h3>
+        <div class="menu-item-counter-container">
+            <div class="menu-item-counter">
+                {{nbr}}
+            </div> 
+            <div class="menu-item-counter-desc">
+                {{desc}}
+            </div> 
+        </div>
+    </div>
+</router-link>
 </template>
 
 <script>
-    export default {
-        components: {
-            PageTitle,
-            EclairageListItem,
+export default {
+        props: [
+            'title',
+            'nbr',
+            'desc',
+            'route',
+            'iconName',
+        ],
+        computed: {
+            src () {
+                return require('../../assets/icons/' + this.iconName + '.svg')
+            }
         },
-        data() {
-            return {}
+        data () {
+            return {
+            }
         },
-        created() {},
-        methods: {}
+        created () {
+        },
+        methods: {
+        }
     }
 </script>
 
 <style scoped>
+h3 {
+    margin: 0;
+    margin-bottom: 10px;
+}
 
+.menu-list-item {
+    margin: 3px 16px;
+    background-color: white;
+    display: flex;
+    height: 70px;
+    border-radius: 3px;
+    overflow: hidden;
+    box-shadow: 0 2px 3px 0 rgba(0,0,0,.2);
+    cursor: pointer;
+    transition: all .3s ease-in-out;
+}
+
+.menu-list-item:active {
+    background-color: #ddd;
+}
+
+.menu-item-icon-section {
+    width: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 2px 0 10px 0 rgba(0,0,0,0.05);
+}
+
+.menu-item-content {
+    padding: 5px 10px;
+    font-family: AvenirNext-Medium;
+    font-size: 15px;
+    color: #465565;
+    display: flex;
+    flex-direction: column;
+}
+
+.menu-item-counter-container {
+    display: flex;
+    font-family: AvenirNext-Regular;
+    font-size: 12px;
+    color: #586E75;
+    align-items: center;
+}
+
+.menu-item-counter {
+    height: 20px;
+    width: 20px;
+    background-color: #6FB485;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 5px;
+}
+
+img {
+    width: 30px;
+}
 </style>
