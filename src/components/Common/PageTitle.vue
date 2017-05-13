de94e46c4855534b3c18e28a812f4764beb57572
Mi2pain <hug93@hotmail.fr>
2017-05-13 19:26:08 +0200

    Composant PageTitle

---
+++ src/components/Common/PageTitle.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="pageTitle">
+    <div class="titleImage">
+        <img :src="titleImage"></img>
+    </div>
+    <h1><slot></slot></h1>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['titleImage'],
+        data() {
+            return {}
+        },
+    }
+</script>
+
+<style scoped>
+    .pageTitle {
+        background-color: #465565;
+    }
+    
+    .titleImage {
+        background-color: #6FB485;
+    }
+</style>
