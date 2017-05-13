170e3d13eaaa15bfdde7ed2951f2475b4753bc3e
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2017-05-14 00:15:37 +0200

    ADD: router transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <vue-me-header/>
-    <router-view></router-view>
+    <transition name="slide-fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <voice-recognizer/>
   </div>
 </template>
@@ -29,5 +31,16 @@ html, body {
   min-height: 100vh;
   margin: 0;
   background-color: #F5F5F5;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .2s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(-320px);
+  opacity: 0;
 }
 </style>
