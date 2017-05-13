b0b6a4a13d30916313e09c0146ff280487f8f442
Mi2pain <hug93@hotmail.fr>
2017-05-13 22:56:14 +0200

    ajout toggle file

---
+++ src/components/Eclairage/Toggle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="toggle" v-on="click: switch" v-class="active: isActive"></div>
+</template>
+
+<script>
+    export default {
+        data() {
+            isActive: false;
+        },
+        methods: {
+            switch: function() {
+                this.isActive = !this.isActive;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .toggle {}
+    
+    .title:active {}
+</style>
