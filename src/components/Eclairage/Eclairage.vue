f386c4f9b5aef465a8ef4f32b4f3cb0eff14b606
Mi2pain <hug93@hotmail.fr>
2017-05-13 19:27:38 +0200

    Init Option

--- src/components/Eclairage/Eclairage.vue
+++ src/components/Eclairage/Eclairage.vue
@@ -1,14 +1,21 @@
 <template>
 <div>
     <page-title titleImage="">Eclairage</page-title>
+    <ul>
+        <li v-for="option in options">
+            <option :option="option"></option>
+        </li>
+    </ul>
 </div>
 </template>
 
 <script>
     import PageTitle from '../Common/PageTitle'
+    import Option from './Option'
     export default {
         components: {
             PageTitle,
+            Option,
         },
         data() {
             return {}
---
+++ src/components/Eclairage/Option.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="pageTitle">
+    <h3 class ="title">{{ option.title }}</h3>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['option'],
+        data() {
+            return {}
+        },
+    }
+</script>
+
+<style scoped>
+    .title {
+        color: #465565;
+    }
+    
+    .title:hover {
+        color: #6FB485;
+    }
+</style>
